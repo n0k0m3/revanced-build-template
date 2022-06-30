7a3616ab4c763f5cb628938ca08525978bc47681
MatadorProBr <71022656+MatadorProBr@users.noreply.github.com>
2022-06-30 19:36:43 +0600

    Change the YT version

--- build_revanced.sh
+++ build_revanced.sh
@@ -2,11 +2,11 @@
 
 # Latest compatible version of apks
 # YouTube Music 5.03.50
-# YouTube 17.24.35
+# YouTube 17.25.34
 # Vanced microG 0.2.24.220220
 
 YTM_VERSION="5.03.50"
-YT_VERSION="17.24.35"
+YT_VERSION="17.25.34"
 VMG_VERSION="0.2.24.220220"
 
 # Artifacts associative array aka dictionary
@@ -60,7 +60,7 @@ echo "Building YouTube APK"
 echo "************************************"
 
 mkdir -p build
-# Obtained from: revanced-patches-2.1.0
+# Obtained from: revanced-patches-2.3.1
 # available_patches="-e amoled -e minimized-playback -e disable-create-button -e premium-heading -e custom-branding -e disable-shorts-button -e disable-fullscreen-panels -e old-quality-layout -e hide-cast-button -e microg-support -e general-ads -e video-ads -e seekbar-tapping -e upgrade-button-remover -e tasteBuilder-remover -e background-play -e exclusive-audio-playback -e codecs-unlock -e hide-watermark -e custom-playback-speed -e hide-autoplay-button"
 
 if [ -f "com.google.android.youtube.apk" ]

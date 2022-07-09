2b1feefcbedd25d7fbe5a850237f534363516b38
Ira Abramov <github@ira.abramov.org>
2022-07-09 11:13:53 +0300

    since .editorconfig was removed, I am hardcoding shfmt to indent by 4 spaces.

--- build_revanced.sh
+++ build_revanced.sh
@@ -26,29 +26,29 @@ artifacts["apkeep"]="EFForg/apkeep apkeep-x86_64-unknown-linux-gnu"
 ## Functions
 
 get_artifact_download_url() {
-  # Usage: get_download_url <repo_name> <artifact_name> <file_type>
-  local api_url result
-  api_url="https://api.github.com/repos/$1/releases/latest"
-  # shellcheck disable=SC2086
-  result=$(curl $api_url | jq ".assets[] | select(.name | contains(\"$2\") and contains(\"$3\") and (contains(\".sig\") | not)) | .browser_download_url")
-  echo "${result:1:-1}"
+    # Usage: get_download_url <repo_name> <artifact_name> <file_type>
+    local api_url result
+    api_url="https://api.github.com/repos/$1/releases/latest"
+    # shellcheck disable=SC2086
+    result=$(curl $api_url | jq ".assets[] | select(.name | contains(\"$2\") and contains(\"$3\") and (contains(\".sig\") | not)) | .browser_download_url")
+    echo "${result:1:-1}"
 }
 
 ## Main
 
 # cleanup to fetch new revanced on next run
 if [[ "$1" == "clean" ]]; then
-  rm -f revanced-cli.jar revanced-integrations.apk revanced-patches.jar
-  exit
+    rm -f revanced-cli.jar revanced-integrations.apk revanced-patches.jar
+    exit
 fi
 
 # Fetch all the dependencies
 for artifact in "${!artifacts[@]}"; do
-  if [ ! -f "$artifact" ]; then
-    echo "Downloading $artifact"
-    # shellcheck disable=SC2086,SC2046
-    curl -L -o "$artifact" $(get_artifact_download_url ${artifacts[$artifact]})
-  fi
+    if [ ! -f "$artifact" ]; then
+        echo "Downloading $artifact"
+        # shellcheck disable=SC2086,SC2046
+        curl -L -o "$artifact" $(get_artifact_download_url ${artifacts[$artifact]})
+    fi
 done
 
 # Fetch microG
@@ -56,15 +56,15 @@ chmod +x apkeep
 
 # get latest YouTube packages if the user asks
 if [[ "$1" == "get_yt" ]]; then
-  echo "Downloading youtube"
-  ./apkeep -a com.google.android.youtube@${YT_VERSION} com.google.android.youtube
-  ./apkeep -a com.google.android.apps.youtube.music@${YTM_VERSION} com.google.android.apps.youtube.music
+    echo "Downloading youtube"
+    ./apkeep -a com.google.android.youtube@${YT_VERSION} com.google.android.youtube
+    ./apkeep -a com.google.android.apps.youtube.music@${YTM_VERSION} com.google.android.apps.youtube.music
 fi
 
 if [ ! -f "vanced-microG.apk" ]; then
-  echo "Downloading Vanced microG"
-  ./apkeep -a com.mgoogle.android.gms@$VMG_VERSION .
-  mv com.mgoogle.android.gms@$VMG_VERSION.apk vanced-microG.apk
+    echo "Downloading Vanced microG"
+    ./apkeep -a com.mgoogle.android.gms@$VMG_VERSION .
+    mv com.mgoogle.android.gms@$VMG_VERSION.apk vanced-microG.apk
 fi
 
 # if [ -f "com.google.android.youtube.xapk" ]
@@ -88,38 +88,38 @@ mkdir -p build
 
 # Check if there is anything in patches which does NOT start with a hash
 if grep -q '^[^#]' $patches; then
-  # If yes, output from grep command below is fed into read which assign it to patch & ultimately store it in our array of excluded_patches
-  # Note: 'read' reads until it hits a newline, grep preserves newline. Thus, we get all patches in one huge chunk & read reads them one by one until EOF
-  while read -r patch; do
-    excluded_patches+=("-e $patch")
-  done < <(grep '^[^#]' $patches)
+    # If yes, output from grep command below is fed into read which assign it to patch & ultimately store it in our array of excluded_patches
+    # Note: 'read' reads until it hits a newline, grep preserves newline. Thus, we get all patches in one huge chunk & read reads them one by one until EOF
+    while read -r patch; do
+        excluded_patches+=("-e $patch")
+    done < <(grep '^[^#]' $patches)
 fi
 
 if [ -f "com.google.android.youtube.apk" ]; then
-  echo "Building Root APK"
-  java -jar revanced-cli.jar -m revanced-integrations.apk -b revanced-patches.jar --mount \
-    -e microg-support ${excluded_patches[@]} \
-    -a com.google.android.youtube.apk -o build/revanced-root.apk
-  echo "Building Non-root APK"
-  java -jar revanced-cli.jar -m revanced-integrations.apk -b revanced-patches.jar \
-    ${excluded_patches[@]} \
-    -a com.google.android.youtube.apk -o build/revanced-nonroot.apk
+    echo "Building Root APK"
+    java -jar revanced-cli.jar -m revanced-integrations.apk -b revanced-patches.jar --mount \
+        -e microg-support ${excluded_patches[@]} \
+        -a com.google.android.youtube.apk -o build/revanced-root.apk
+    echo "Building Non-root APK"
+    java -jar revanced-cli.jar -m revanced-integrations.apk -b revanced-patches.jar \
+        ${excluded_patches[@]} \
+        -a com.google.android.youtube.apk -o build/revanced-nonroot.apk
 else
-  echo "Cannot find YouTube APK, skipping build"
+    echo "Cannot find YouTube APK, skipping build"
 fi
 echo ""
 echo "************************************"
 echo "Building YouTube Music APK"
 echo "************************************"
 if [ -f "com.google.android.apps.youtube.music.apk" ]; then
-  echo "Building Root APK"
-  java -jar revanced-cli.jar -b revanced-patches.jar --mount \
-    -e microg-support ${excluded_patches[@]} \
-    -a com.google.android.apps.youtube.music.apk -o build/revanced-music-root.apk
-  echo "Building Non-root APK"
-  java -jar revanced-cli.jar -b revanced-patches.jar \
-    ${excluded_patches[@]} \
-    -a com.google.android.apps.youtube.music.apk -o build/revanced-music-nonroot.apk
+    echo "Building Root APK"
+    java -jar revanced-cli.jar -b revanced-patches.jar --mount \
+        -e microg-support ${excluded_patches[@]} \
+        -a com.google.android.apps.youtube.music.apk -o build/revanced-music-root.apk
+    echo "Building Non-root APK"
+    java -jar revanced-cli.jar -b revanced-patches.jar \
+        ${excluded_patches[@]} \
+        -a com.google.android.apps.youtube.music.apk -o build/revanced-music-nonroot.apk
 else
-  echo "Cannot find YouTube Music APK, skipping build"
+    echo "Cannot find YouTube Music APK, skipping build"
 fi

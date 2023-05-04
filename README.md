99fd253716874cc2fef9627087f9757dafde75cb
Minh Vương <hardingadonis@gmail.com>
2023-05-04 10:47:53 +0700

    Use Python to write script for downloading APK files

--- README.md
+++ README.md
@@ -1,24 +1,3 @@
-# Revanced Build
-This repo template will allow you to build ReVanced using Github Actions. This will helps people who don't want to setup build environments on their machines.
+# Revanced Auto Build
+> Auto build Revanced for individual 😎😎
 
-## Notes
-- The script will download the **selected compatible version**([see here](versions.json)) of Youtube on APKMirror, **NOT** latest official version on Google Play.
-- Under **NO CIRCUMSTANCES** any APKs will be uploaded to this repository to avoid DMCA.
-
-## How to setup
-1. Fork or create a new repository using this repository as a template ([Guide](https://docs.github.com/en/repositories/creating-and-managing-repositories/creating-a-repository-from-a-template)). DO NOT FORK if you need to set the new repo to private.
-2. That's it! You can now build ReVanced using Github Actions.
-
-## Customize your build
-If you wish to continue with the default settings, you may skip this step.
-
-All supported ReVanced apps are built by default by the script. If you wish to modify this behaviour and build only the apps that you specify, edit the 'build.targets' file with your preferred text editor. For the apps that you wish to be built, set the value of variable associated with the app to "true". Any variable with a value that is not "true" will be skip the associated app from being built.
-
-For example, if you wish to skip TikTok ReVanced from being built, change the value of TIKTOK_NONROOT to "false".
-
-By default this will build ReVanced apps with ALL available patches. Follow [this guide](PATCHES_GUIDE.md) to exclude/customizing patches for your build.
-
-## How to build
-1. Go to Actions -> All workflows -> ReVanced Build ([Example](images/workflow_run.png))
-2. Run the `build` workflow (try to use `experimental_build` if the logs show not all patches applied correctly while revanced is not yet released)
-3. Download the APKs from the draft releases ([Example](images/build_release.png))
---
+++ auto-build.py
@@ -0,0 +1,96 @@
+import urllib.request
+import requests
+import yaml
+import os
+
+from bs4 import BeautifulSoup
+
+# Define class Packages
+class Package:
+    def __init__(self, package_name, version, uptodown, exclude_options):
+        self.package_name = package_name
+        self.version = version
+        self.uptodown = uptodown
+        self.exclude_options = exclude_options or []
+
+# Load data from yaml file
+def load_data_from_yaml_file(name_yaml_file):
+    print('===== Load packages from', name_yaml_file, '=====')
+
+    with open(name_yaml_file, 'r') as f:
+        packages_data = yaml.safe_load(f)
+    
+    packages = []
+
+    # Get `package_name`, `version`, `uptodown`, `exclude_options` for each package
+    for package in packages_data.items():
+        package_name = None
+        version = None
+        uptodown = None
+        exclude_options = None
+
+        # Get `package_name`
+        package_name = package[0]
+
+        # Get `version`
+        version = package[1][0].get('version')
+
+        # Get `uptodown`
+        uptodown = package[1][1].get('uptodown')
+
+        # If greater than 2, get more of `exclude_options`
+        if len(package[1]) > 2:
+            exclude_options = ' '.join(package[1][2].get('exclude_options'))
+        
+        packages.append(Package(package_name, version, uptodown, exclude_options))
+
+        print('- Loaded:')
+        print('\t- package_name:', package_name)
+        print('\t- version:', version)
+        print('\t- uptodown:', uptodown)
+        print('\t- exclude_options:', exclude_options)
+    
+    return packages
+
+# Download package
+def download_package(package, folder_to_save):
+    print('- Downloading', package.package_name, 'version', package.version)
+
+    response = requests.get(package.uptodown)
+    soup = BeautifulSoup(response.content, 'html.parser')
+
+    version = soup.find('div', {'class': 'version'}).text
+
+    if version == package.version:
+        name_file = package.package_name + '-' + package.version.replace('.', '-') + '.apk'
+
+        if not os.path.exists(folder_to_save + '/' + name_file):
+            # Get URL of APK file
+            url = soup.find('button', {'id': 'detail-download-button'})['data-url']
+
+            print('\t- URL:', url)
+            print('\t- Downloading', package.package_name, 'to\'', folder_to_save, '\' folder...')
+
+            urllib.request.urlretrieve(url, os.path.join(folder_to_save, name_file))
+        else:
+            print('\t- Notification:', name_file, 'already exists')
+    else:
+        print('- Error:', package.version, 'not same with', version)
+        return
+    
+    print('- Done', package.package_name, 'version', package.version)
+
+# Main
+if __name__ == "__main__":
+    # Load data from yaml file
+    packages = load_data_from_yaml_file('packages.yml')
+
+    # Create `downloaded` folder
+    if not os.path.exists('downloaded'):
+        os.makedirs('downloaded')
+        print('===== Create \'downloaded\' folder =====')
+    
+    # Download each package to `download` folder
+    print('===== Download to \'downloaded\' folder =====')
+    for package in packages:
+        download_package(package, 'downloaded') 
